d8ca8555e820e5197259d7897d8f35018925394d
krymkovdv <s89232844224@gmail.com>
2024-10-22 21:36:43 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,7 +1,10 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 count_of_numbers = len(numbers)
-Error_number = numbers[4]
-numbers_new = numbers[:4]+numbers[5:]
-sum_of_numbers = sum(numbers_new)
-numbers[4] = sum_of_numbers / count_of_numbers
+for i in range(len(numbers)):
+    if numbers[i] == None:
+        sum_of_numbers = sum(numbers[:i] + numbers[i+1:])
+        numbers[i] = sum_of_numbers / count_of_numbers
+        break
+    else:
+        i+=1
 print("Измененный список:", numbers)
