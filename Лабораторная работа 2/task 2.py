56a99271ef96687e7fca479a06e1b0c276958810
krymkovdv <s89232844224@gmail.com>
2024-10-30 21:20:04 +0300

    Update task 2.py

--- Лабораторная работа 2/task 2.py
+++ Лабораторная работа 2/task 2.py
@@ -2,11 +2,8 @@ salary = 5000  # Ежемесячная зарплата
 spend = 6000  # Траты за первый месяц
 months = 10  # Количество месяцев, которое планируется протянуть без долгов
 increase = 0.03  # Ежемесячный рост цен
-sum_spend = 0
-total_salary = 0
+money_capital = 0
 for i in range(months):
-    sum_spend += spend
-    spend = spend * (1+increase)
-    total_salary = (i+1)*salary
-    money_capital = sum_spend - total_salary
+    money_capital += (spend - salary)
+    spend += spend * increase
 print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
